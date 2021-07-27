3e77ee12b13974ac88ecdc03418befb0695648a8
Seokkitdo <sjd12329@gmail.com>
2021-07-27 23:44:24 +0900

    styles-components Separation

---
+++ src/components/Card/Card.styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const CardLayout = styled.div`
+  font-family: "SF Pro Display", sans-serif;
+  width: 500px;
+  font-size: 18px;
+  margin: 12px 0;
+  background: #f8f9fa;
+  border: 0.5px solid #ced4da;
+  box-sizing: border-box;
+  border-radius: 20px;
+`;
+
+export const FlexElements = styled.div`
+  display: flex;
+  align-items: baseline;
+`;
+export const CommentId = styled.div`
+  margin: 20px 12px 12px 20px;
+  height: 21px;
+  font-weight: 700;
+  line-height: 21px;
+  color: #212529;
+`;
+
+export const Email = styled.div`
+  height: 21px;
+  margin: 0 12px 12px 20px;
+  font-weight: 700;
+  line-height: 21px;
+  color: #212529;
+`;
+
+export const CommonText = styled.div`
+  height: 21px;
+  font-weight: 400;
+  line-height: 21px;
+`;
+
+export const CommentTitle = styled.div`
+  height: 21px;
+  line-height: 21px;
+  font-weight: 700;
+  margin: 0 0 2px 20px;
+`;
+
+export const Content = styled.div`
+  margin: 0px 20px 20px;
+  line-height: 21px;
+  font-weight: 400;
+`;
